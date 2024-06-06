07743f601dd640ee14df1a3ae93e150ee7a8045d
RSanchez19 <00191018@uca.edu.sv>
2024-06-05 22:12:20 -0600

    fixing some bugs

--- src/app/guardViews/scan-mode/scan-mode.component.ts
+++ src/app/guardViews/scan-mode/scan-mode.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BarcodeFormat } from '@zxing/library';
 
 @Component({
   selector: 'app-scan-mode',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ZXingScannerModule],
   templateUrl: './scan-mode.component.html',
-  styleUrl: './scan-mode.component.css'
+  styleUrl: './scan-mode.component.css',
 })
 export class ScanModeComponent {
+  formats: BarcodeFormat[] = [BarcodeFormat.QR_CODE];
 
+  handleQrCodeResult(result: string) {
+    console.log('QR code result:', result);
+  }
 }
